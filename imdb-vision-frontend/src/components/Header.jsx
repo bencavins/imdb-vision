7630d3a979dcfbd88701fd214f4c68657807c54f
Ben Cavins <ben.cavins@flatironschool.com>
2023-07-18 14:00:26 -0400

    add simple react router dom

---
+++ imdb-vision-frontend/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div id="header">
+      <h1>IMDB Vision</h1>
+    </div>
+  )
+}
--- imdb-vision-frontend/src/main.jsx
+++ imdb-vision-frontend/src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+// import App from './App.jsx'
 import './index.css'
+import Root from "./routes/root"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
